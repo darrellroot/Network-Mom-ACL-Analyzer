9246fc0b7cee1ed604579a488ceed752e2a76d14
Darrell Root <darrellrootjunk@mac.com>
2019-05-13 13:11:49 -0700

    Initial Commit

---
+++ Network Mom ACL Analyzer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Network Mom ACL Analyzer
+//
+//  Created by Darrell Root on 5/13/19.
+//  Copyright © 2019 Network Mom LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
