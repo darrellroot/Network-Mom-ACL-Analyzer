07942f243c4477223d0fedf96587eefb43c95202
Darrell Root <darrellrootjunk@mac.com>
2019-06-17 12:30:04 -0700

    Added expiration date, about window, automatic spawning of about window, app sandbox settings.

--- Network Mom ACL Analyzer/AppDelegate.swift
+++ Network Mom ACL Analyzer/AppDelegate.swift
@@ -11,13 +11,24 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    let expirationDateComponents = DateComponents(calendar: Calendar.current, timeZone: .current, era: nil, year: 2019, month: 8, day: 1, hour: 1, minute: 1, second: 1, nanosecond: nil, weekday: nil, weekdayOrdinal: nil, quarter: nil, weekOfMonth: nil, weekOfYear: nil, yearForWeekOfYear: nil)
     var analyzeDashboardControllers: [AnalyzeDashboardController] = []
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-        let analyzeDashboardController = AnalyzeDashboardController()
-        analyzeDashboardControllers.append(analyzeDashboardController)
-        analyzeDashboardController.showWindow(self)
+        
+        
+        let expirationDate = Calendar.current.nextDate(after: Date(timeIntervalSinceReferenceDate: 0), matching: expirationDateComponents, matchingPolicy: .nextTime)
+        
+        if let expirationDate = expirationDate, expirationDate > Date() {
+            let analyzeDashboardController = AnalyzeDashboardController()
+            analyzeDashboardControllers.append(analyzeDashboardController)
+            analyzeDashboardController.showWindow(self)
+        } else {
+            expiredAlert()
+        }
+        
+        NSApplication.shared.orderFrontStandardAboutPanel(self)
+
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -25,9 +36,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     @IBAction func newAclAnalyzer(_ sender: NSMenuItem) {
-        let analyzeDashboardController = AnalyzeDashboardController()
-        analyzeDashboardControllers.append(analyzeDashboardController)
-        analyzeDashboardController.showWindow(self)
+        let expirationDate = Calendar.current.nextDate(after: Date(timeIntervalSinceReferenceDate: 0), matching: expirationDateComponents, matchingPolicy: .nextTime)
+        if let expirationDate = expirationDate, expirationDate > Date() {
+            let analyzeDashboardController = AnalyzeDashboardController()
+            analyzeDashboardControllers.append(analyzeDashboardController)
+            analyzeDashboardController.showWindow(self)
+        } else {
+            expiredAlert()
+        }
+    }
+    func expiredAlert() {
+        let alert = NSAlert()
+        alert.messageText = "This early alpha version of Network Mom ACL Analyzer has expired"
+        alert.informativeText = "Go to https://networkmom.net/acl or (eventually) the MacOS 10.14 Mojave or MacOS 10.15 Catalina app store"
+        alert.addButton(withTitle: "OK")
+        alert.runModal()
     }
 }
 
