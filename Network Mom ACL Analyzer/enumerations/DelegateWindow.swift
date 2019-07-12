53930bf4937f85b8e43efebf0fddcded3875eec1
Darrell Root <darrellrootjunk@mac.com>
2019-07-12 13:46:22 -0700

    adding some files missed in recent commit.  version 1.1 RC1

---
+++ Network Mom ACL Analyzer/enumerations/DelegateWindow.swift
@@ -0,0 +1,16 @@
+//
+//  DelegateWindow.swift
+//  Network Mom ACL Analyzer
+//
+//  Created by Darrell Root on 7/11/19.
+//  Copyright © 2019 Network Mom LLC. All rights reserved.
+//
+
+import Foundation
+
+enum DelegateWindow {
+    case ingressValidation
+    case egressValidation
+    case ingressAnalyze
+    case egressAnalyze
+}
