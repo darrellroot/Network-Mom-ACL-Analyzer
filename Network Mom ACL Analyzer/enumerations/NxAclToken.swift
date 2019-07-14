649547d40fee8348de2f724c505691846b621d05
Darrell Root <darrellrootjunk@mac.com>
2019-07-14 08:58:39 -0700

    nxos working but not with object groups

---
+++ Network Mom ACL Analyzer/enumerations/NxAclToken.swift
@@ -0,0 +1,62 @@
+//
+//  AclToken.swift
+//  Network Mom ACL Analyzer
+//
+//  Created by Darrell Root on 6/8/19.
+//  Copyright © 2019 Network Mom LLC. All rights reserved.
+//
+
+import Foundation
+
+enum NxAclToken: Equatable {
+    case action(AclAction)
+    case ipProtocol(UInt)
+    case any
+    case host
+    case portOperator(PortOperator)
+    case comment
+    case established
+    case fourOctet(UInt)
+    case cidr(IpRange)
+    case number(UInt)
+    case name(String)
+    
+    init?(string: String) {
+        switch string {
+        case "permit":
+            self = .action(.permit)
+        case "deny":
+            self = .action(.deny)
+        case "ahp","eigrp","esp","gre","icmp","igmp","ip","nos","ospf","pcp","pim","tcp","udp":
+            if let ipProtocol = string.nxosIpProtocol {
+                self = .ipProtocol(ipProtocol)
+            } else {
+                return nil
+            }
+        case "any":
+            self = .any
+        case "host":
+            self = .host
+        case "eq","gt","lt","neq","range":
+            if let portOperator = PortOperator(string) {
+                self = .portOperator(portOperator)
+            } else {
+                return nil
+            }
+        case "established","est":
+            self = .established
+        default:
+            if let number = UInt(string) {
+                self = .number(number)
+            } else if let ipRange = IpRange(cidr: string) {
+                self = .cidr(ipRange)
+            } else if let ipv4Address = string.ipv4address {
+                self = .fourOctet(ipv4Address)
+            } else if string.first == "!" || string.first == "#" || string.first == ";" || string.first == ":" {
+                self = .comment
+            } else  {
+                self = .name(string)
+            }
+        }
+    }
+}
