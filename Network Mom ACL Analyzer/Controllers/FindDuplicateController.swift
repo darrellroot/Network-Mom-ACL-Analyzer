a1f270bec6a5a7366e33dcce2d193f5d4edde4be
Darrell Root <darrellrootjunk@mac.com>
2019-07-23 13:51:19 -0700

    starting find duplicate controller

---
+++ Network Mom ACL Analyzer/Controllers/FindDuplicateController.swift
@@ -0,0 +1,19 @@
+//
+//  FindDuplicateController.swift
+//  Network Mom ACL Analyzer
+//
+//  Created by Darrell Root on 7/23/19.
+//  Copyright © 2019 Network Mom LLC. All rights reserved.
+//
+
+import Cocoa
+
+class FindDuplicateController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
